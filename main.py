32386aceb228f1080c3bd6ea31141f9a2f57f276
Corie Rhodes <carhodes@deakin.edu.au>
2020-12-03 15:58:44 +1100

    Uploaded main.py and movies.txt

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL).text
+
+soup = BeautifulSoup(response, 'html.parser')
+
+all_movies = soup.find_all(name='h3', class_='title')
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
+print(movies)
+
+
+
